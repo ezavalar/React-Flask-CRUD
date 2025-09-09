ea96422b0829ed506ae97f5e721e16e83cef9512
Ericka Zavala <contacto@ezavalar.com>
2025-09-08 19:34:52 -0600

    Agregar Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { About } from './components/About';
 import { Users } from './components/Users';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <Router>   
+      <Navbar />
       <div>
         <Routes>
           <Route path="/about" element={<About />} />
